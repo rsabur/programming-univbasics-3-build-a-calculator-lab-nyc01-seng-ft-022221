b7cf1a9b15b20666876d3ec505e32bfb83d809f4
Radiyah Abdus-Sabur <rsabur58@gmail.com>
2021-01-25 22:12:46 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,18 +1,25 @@
-def addition(num1, num2)
+num1 = 3
+num2 = 4
 
+def addition(num1, num2)
+  num1 + num2
 end
+addition
 
 def subtraction(num1, num2)
-
+  num2 - num1
 end
+subtraction
 
 def division(num1, num2)
-
+  num2 / num1
 end
+division
 
 def multiplication(num1, num2)
-
+  num1 * num2
 end
+multiplication
 
 def modulo(num1, num2)
 
