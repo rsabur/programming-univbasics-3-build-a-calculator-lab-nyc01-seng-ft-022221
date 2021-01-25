ce2667b47c5b58eb30f522460910bfe56f709ce3
Radiyah Abdus-Sabur <rsabur58@gmail.com>
2021-01-25 22:15:43 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,33 +1,24 @@
-num1 = 3
-num2 = 4
-num = 9
-
 def addition(num1, num2)
-  return num1 + num2
+  num1 + num2
 end
-addition
+addition(3,4)
 
 def subtraction(num1, num2)
-  return num2 - num1
+
 end
-subtraction
 
 def division(num1, num2)
-  return num2 / num1
+
 end
-division
 
 def multiplication(num1, num2)
-  return num1 * num2
+
 end
-multiplication
 
 def modulo(num1, num2)
-  return num2 % num1
+
 end
-modulo
 
 def square_root(num)
-  return Math.sqrt(num)
+
 end
-square_root
